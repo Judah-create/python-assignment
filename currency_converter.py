4d7de368a67bc51e17acc785d074bf74a2e9218d
Judah-create <rgrim029@gmail.com>
2026-01-15 22:51:10 +0100

    Currency Converter

---
+++ currency_converter.py
@@ -0,0 +1,13 @@
+naira = float(input("Enter amount in naira: "))
+
+dollar = naira / 1500
+euro = naira / 1600
+pound = naira / 1900
+cedi = naira / 120
+canadian = naira / 1100
+
+print("Dollar:", dollar)
+print("Euro:", euro)
+print("Pound:", pound)
+print("Cedi:", cedi)
+print("Canadian Dollar:", canadian)
